a7c10236e40fef3fb95cf16a027e2cb9bb84b238
Valdiney França <valdiney.2@hotmail.com>
2013-07-03 14:39:13 -0300

    Porcentagem com php

--- index.php
+++ index.php
@@ -4,6 +4,10 @@ $valor = 850;
 $percent = $valor * 15 / 100;
 echo " Vai receber 15% de".$valor." que é ".$percent;
 
+ function porcentagem($valor,$percent){
+  $mensagem = " Vai receber 15% de".$valor." que é ".$percent;
+  return $mensagem.$valor * $percent / 100;
+ }
 
-
+ porcentagem(850,15);
 ?>
